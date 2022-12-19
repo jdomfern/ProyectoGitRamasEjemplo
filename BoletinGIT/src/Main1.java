4c0d4b877ed288f07f12e57ee9748cfe5d203ed7
Jorge <Jorge@DESKTOP-5FAD0MS>
2022-12-19 20:01:34 +0100

    Se cambia hola

--- BoletinGIT/src/Main1.java
+++ BoletinGIT/src/Main1.java
@@ -3,7 +3,7 @@ public class Main1 {
 
 	public static void main(String[] args) {
 		// Boletin GIT ejercicio 3  segundo commit
-		System.out.println("Hello mundo");
+		System.out.println("Hola mundo");
 
 	}
 
