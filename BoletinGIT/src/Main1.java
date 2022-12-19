627e4cc2256bc8e29277e5f2bad931066f78513a
Jorge <Jorge@DESKTOP-5FAD0MS>
2022-12-19 20:00:34 +0100

    se añade mundo

--- BoletinGIT/src/Main1.java
+++ BoletinGIT/src/Main1.java
@@ -3,7 +3,7 @@ public class Main1 {
 
 	public static void main(String[] args) {
 		// Boletin GIT ejercicio 3  segundo commit
-		System.out.println("Hello");
+		System.out.println("Hello mundo");
 
 	}
 
